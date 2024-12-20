85115dbbe489ca70aeaf0558072cd36bb7695f3c
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-12-20 19:25:06 +0530

    Update changetime.py

--- ROYEDITX/modules/changetime.py
+++ ROYEDITX/modules/changetime.py
@@ -1,43 +1,50 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, filters
-from pymongo import MongoClient, ReturnDocument
-from motor.motor_asyncio import AsyncIOMotorClient 
-from ROYEDITX import application 
-from ROYEDITX import db, collection, user_totals_collection, user_collection, top_global_groups_collection, top_global_groups_collection, group_user_totals_collection
+from pymongo import  ReturnDocument
+from pyrogram.enums import ChatMemberStatus, ChatType
+from ROYEDITX import user_totals_collection, ROY
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-async def change_time(update: Update, context: CallbackContext) -> None:
+
+
+
+ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
+
+
+
+
+
+@ROY.on_message(filters.command("changetime"))
+async def change_time(client: Client, message: Message):
     
-    user = update.effective_user
-    chat = update.effective_chat
-    member = await chat.get_member(user.id)
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    member = await shivuu.get_chat_member(chat_id,user_id)
+        
 
-    if member.status not in ('administrator', 'creator'):
-        await update.message.reply_text('❖ ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ʀɪɢʜᴛs ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴍᴅs.')
+    if member.status not in ADMINS :
+        await message.reply_text('You are not an Admin.')
         return
+
     try:
-        
-        args = context.args
-        if len(args) != 1:
-            await update.message.reply_text('❖ ɪɴᴄᴏʀʀᴇᴄᴛ ғᴏʀᴍᴀᴛ, ᴘʟᴇᴀsᴇ ᴜsᴇ ➥ /changetime ɴᴜᴍʙᴇʀ')
+        args = message.command
+        if len(args) != 2:
+            await message.reply_text('Please use: /changetime NUMBER')
             return
 
-        
-        new_frequency = int(args[0])
-        if new_frequency < 1:
-            await update.message.reply_text('❖ ᴛʜᴇ ᴍᴇssᴀɢᴇ ғʀᴇǫᴜᴇɴᴄʏ ᴍᴜsᴛ ʙᴇ ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ ᴏʀ ᴇǫᴜᴀʟ ᴛᴏ 100.')
+        new_frequency = int(args[1])
+        if new_frequency < 70:
+            await message.reply_text('The message frequency must be greater than or equal to 70.')
             return
 
-        
+    
         chat_frequency = await user_totals_collection.find_one_and_update(
-            {'chat_id': str(chat.id)},
+            {'chat_id': str(chat_id)},
             {'$set': {'message_frequency': new_frequency}},
             upsert=True,
             return_document=ReturnDocument.AFTER
         )
 
-        await update.message.reply_text(f'❖ sᴜᴄᴄᴇssғᴜʟʟʏ ᴄʜᴀɴɢᴇᴅ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴀᴘᴘᴇᴀʀᴀɴᴄᴇ ғʀᴇǫᴜᴇɴᴄʏ ᴛᴏ ᴇᴠᴇʀʏ {new_frequency} ᴍᴇssᴀɢᴇ.')
+        await message.reply_text(f'Successfully changed {new_frequency}')
     except Exception as e:
-        await update.message.reply_text("❖ ғᴀɪʟᴇᴅ ᴛᴏ ᴄʜᴀɴɢᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ ᴀᴘᴘᴇᴀʀᴀɴᴄᴇ ғʀᴇǫᴜᴇɴᴄʏ.")
-
-
-application.add_handler(CommandHandler("changetime", change_time, block=False))
+        await message.reply_text(f'Failed to change {str(e)}')
+        
